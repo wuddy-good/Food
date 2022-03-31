859d05255ec9f2b7d8fe47ceabc976918160ff28
Vlad <vladpolisuk127@gmail.com>
2022-03-31 21:26:46 +0300

    fixed tab

--- js/script.js
+++ js/script.js
@@ -1,30 +1,21 @@
-window.addEventListener("DOMContentLoaded", function () {
-  // Tabs
-
-  let tabs = document.querySelectorAll(".tabheader__item"),
+document.addEventListener("DOMContentLoaded", function () {
+  const tabs = document.querySelectorAll(".tabheader__item"),
     tabsContent = document.querySelectorAll(".tabcontent"),
     tabsParent = document.querySelector(".tabheader__items");
-
   function hideTabContent() {
     tabsContent.forEach((item) => {
-      item.classList.add("hide");
-      item.classList.remove("show", "fade");
+      item.style.display = "none";
     });
-
     tabs.forEach((item) => {
       item.classList.remove("tabheader__item_active");
     });
   }
-
   function showTabContent(i = 0) {
-    tabsContent[i].classList.add("show", "fade");
-    tabsContent[i].classList.remove("hide");
+    tabsContent[i].style.display = "block";
     tabs[i].classList.add("tabheader__item_active");
   }
-
   hideTabContent();
   showTabContent();
-
   tabsParent.addEventListener("click", function (event) {
     const target = event.target;
     if (target && target.classList.contains("tabheader__item")) {

fb483d0d1115c9bef34feb8d6b79978d655c6247
Vlad <vladpolisuk127@gmail.com>
2022-04-01 22:40:21 +0300

    work with Rest

--- js/script.js
+++ js/script.js
@@ -138,12 +138,13 @@ window.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("scroll", showModalByScroll);
 
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
+      this.classes = classes;
       this.parent = document.querySelector(parentSelector);
       this.transfer = 27;
       this.changeToUAN();
@@ -153,19 +154,23 @@ window.addEventListener("DOMContentLoaded", function () {
     }
     render() {
       const element = document.createElement("div");
+      if (this.classes.length === 0) {
+        this.element = "menu__item";
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach((className) => element.classList.add(className));
+      }
       element.innerHTML = `
-      <div class="menu__item">
-            <img src=${this.src} alt=${this.alt} />
-            <h3 class="menu__item-subtitle">${this.title}</h3>
-            <div class="menu__item-descr">
-              ${this.descr}
-            </div>
-            <div class="menu__item-divider"></div>
-            <div class="menu__item-price">
-              <div class="menu__item-cost">Цена:</div>
-              <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-            </div>
-        </div>`;
+          <img src=${this.src} alt=${this.alt} />
+          <h3 class="menu__item-subtitle">${this.title}</h3>
+          <div class="menu__item-descr">
+            ${this.descr}
+          </div>
+          <div class="menu__item-divider"></div>
+          <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+          </div>`;
       this.parent.append(element);
     }
   }
@@ -177,7 +182,9 @@ window.addEventListener("DOMContentLoaded", function () {
      свежих овощей и фруктов. Продукт активных и здоровых людей. Это
      бсолютно новый продукт с оптимальной ценой и высоким качеством!`,
     9,
-    ".menu .container"
+    ".menu .container",
+    "menu__item",
+    "big"
   ).render();
   new MenuCard(
     "img/tabs/vegy.jpg",
@@ -187,7 +194,9 @@ window.addEventListener("DOMContentLoaded", function () {
      свежих овощей и фруктов. Продукт активных и здоровых людей. Это
      бсолютно новый продукт с оптимальной ценой и высоким качеством!`,
     9,
-    ".menu .container"
+    ".menu .container",
+    "menu__item",
+    "big"
   ).render();
   new MenuCard(
     "img/tabs/vegy.jpg",
@@ -197,6 +206,8 @@ window.addEventListener("DOMContentLoaded", function () {
      свежих овощей и фруктов. Продукт активных и здоровых людей. Это
      бсолютно новый продукт с оптимальной ценой и высоким качеством!`,
     9,
-    ".menu .container"
+    ".menu .container",
+    "menu__item",
+    "big"
   ).render();
 });

9d2d9b468e14c99e4b52ec1533466affcecb0266
Vlad <vladpolisuk127@gmail.com>
2022-04-01 19:47:06 +0300

    work with class

--- js/script.js
+++ js/script.js
@@ -136,4 +136,57 @@ window.addEventListener("DOMContentLoaded", function () {
     }
   }
   window.addEventListener("scroll", showModalByScroll);
+
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 27;
+      this.changeToUAN();
+    }
+    changeToUAN() {
+      this.price = this.price * this.transfer;
+    }
+    render() {
+      const element = document.createElement("div");
+      element.innerHTML = `
+      <div class="menu__item">
+            <img src=${this.src} alt=${this.alt} />
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">
+              ${this.descr}
+            </div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+              <div class="menu__item-cost">Цена:</div>
+              <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>
+        </div>`;
+      this.parent.append(element);
+    }
+  }
+  new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    `Меню "Фитнес"`,
+    `Меню "Фитнес" - это новый подход к приготовлению блюд: больше
+     свежих овощей и фруктов. Продукт активных и здоровых людей. Это
+     бсолютно новый продукт с оптимальной ценой и высоким качеством!`,
+    9,
+    ".menu .container"
+  ).render();
+  new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    `Меню "Фитнес"`,
+    `Меню "Фитнес" - это новый подход к приготовлению блюд: больше
+     свежих овощей и фруктов. Продукт активных и здоровых людей. Это
+     бсолютно новый продукт с оптимальной ценой и высоким качеством!`,
+    9,
+    ".menu .container"
+  ).render();
 });

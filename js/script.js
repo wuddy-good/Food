6881cecafdfa16fa26194d32c2871de56c6e8fb9
Vlad <vladpolisuk127@gmail.com>
2022-04-01 14:27:46 +0300

    new functionality for modal

--- js/script.js
+++ js/script.js
@@ -95,15 +95,16 @@ window.addEventListener("DOMContentLoaded", function () {
     modal = document.querySelector(".modal"),
     modalCloseBtn = document.querySelector("[data-close]");
 
-  modalTrigger.forEach((btn) => {
-    btn.addEventListener("click", function () {
-      modal.classList.add("show");
-      modal.classList.remove("hide");
-      // Либо вариант с toggle - но тогда назначить класс в верстке
-      document.body.style.overflow = "hidden";
+  function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    // Либо вариант с toggle - но тогда назначить класс в верстке
+    document.body.style.overflow = "hidden";
+    clearInterval(modalTimerId);
+    modalTrigger.forEach((btn) => {
+      btn.addEventListener("click", openModal);
     });
-  });
-
+  }
   function closeModal() {
     modal.classList.add("hide");
     modal.classList.remove("show");
@@ -124,4 +125,15 @@ window.addEventListener("DOMContentLoaded", function () {
       closeModal();
     }
   });
+  const modalTimerId = setTimeout(openModal, 3000);
+  function showModalByScroll() {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight
+    ) {
+      openModal();
+      window.removeEventListener("scroll", showModalByScroll);
+    }
+  }
+  window.addEventListener("scroll", showModalByScroll);
 });
